202ac10adaed03e3804d1ca70fbd33e21d83b1a2
Jordan <Berti@epse.be>
2021-09-16 11:35:48 +0200

    connexion auto session

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,10 @@
 <?php
     session_start();
+    if(isset($_SESSION['level']) AND $_SESSION['level']=="administrateur")
+    {
+        header("LOCATION:dashboard.php");
+    }
+
     if(isset($_POST['username']))
     {
       
